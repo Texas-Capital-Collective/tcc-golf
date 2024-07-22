6b32fd4c9f473537885d687044f4dc269ca77c74
Clint Wang <33455116+txst54@users.noreply.github.com>
2024-07-22 11:20:24 -0500

    more seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://academydallasgolf.netlify.app',
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
